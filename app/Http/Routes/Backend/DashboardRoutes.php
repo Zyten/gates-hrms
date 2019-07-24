b392f23212dad487206583b5dbbefa0bed04989a
Zyten <sruban707@hotmail.com>
2019-07-25 00:21:28 +0800

    Remove * for gh-pages

--- app/Http/Routes/Backend/DashboardRoutes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	Route::group(['middleware' => 'auth'], function () {
-
-		Route::get('/', function () { return redirect()->route('backend.dashboard'); });
-
-		Route::get('dashboard', 'DashboardController@index')->name('backend.dashboard');
-
-	});
--- resources/views/backend/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- backend.footer.blade -->
-<footer class="footer">
-	<div class="footer-block author">
-		<ul>
-		<li> Made with love by <a href="https://zyten.github.io">Ruban Selvarajah</a> </li>
-		</ul>
-	</div>
-</footer>
